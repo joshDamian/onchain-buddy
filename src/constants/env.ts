7c5bf908b5b89daa0b60ba60ed4014045a8e70ef
Joshua Avoaja <avoajajoshua@gmail.com>
2024-09-22 22:04:12 +0100

    debug puppeteer headless setup on server

--- src/constants/env.ts
+++ src/constants/env.ts
@@ -21,6 +21,7 @@ const envSchema = z.object({
     WA_WEBHOOK_VERIFY_TOKEN: notEmptyStringSchema('WA_WEBHOOK_VERIFY_TOKEN'),
     XATA_API_KEY: notEmptyStringSchema('XATA_API_KEY'),
     HOST_URL: z.string().url().default('https://onchain-buddy.onrender.com'),
+    PUPPETEER_EXECUTABLE_PATH: z.string().default(''),
 });
 
 const env = envSchema.parse(process.env);
--- src/utils/page-capture.ts
+++ src/utils/page-capture.ts
@@ -1,8 +1,12 @@
 import puppeteer from 'puppeteer';
+import env from '@/constants/env';
 
 // Todo: Use a global browser instance to avoid launching a new browser for each request
 export const captureAndStorePageScreenshotAsImage = async (url: string, outputPath?: string) => {
-    const browser = await puppeteer.launch({ headless: 'shell' });
+    const browser = await puppeteer.launch({
+        headless: true,
+        executablePath: env.PUPPETEER_EXECUTABLE_PATH ? env.PUPPETEER_EXECUTABLE_PATH : undefined,
+    });
     const page = await browser.newPage();
 
     await page.goto(url, { waitUntil: 'networkidle2' });
@@ -17,7 +21,7 @@ export const captureAndStorePageScreenshotAsImage = async (url: string, outputPa
 };
 
 export const captureAndStorePageScreenshotAsPdf = async (url: string, outputPath?: string) => {
-    const browser = await puppeteer.launch({ headless: 'shell' });
+    const browser = await puppeteer.launch({ headless: true });
     const page = await browser.newPage();
 
     await page.goto(url, { waitUntil: 'networkidle2' });
